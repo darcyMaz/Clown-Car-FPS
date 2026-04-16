3f3b1b15abfb50b1d3ef0d455b24d073babe787e
Hellvangelion <felis.maclellan@gmail.com>
2026-04-15 22:44:06 -0400

    created audio manager + team & credits

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
-
+[RequireComponent(typeof(AudioSource))]
 public class SoundManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   public enum SFX
     {
-        
+        GUNSHOT,
+        SPLAT,
+        TADAA, // plays when the player wins the game
+        THROW
     }
 
-    // Update is called once per frame
+    [SerializeField] private AudioClip[] soundList;
+    private static SoundManager instance;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(SFX sound, float volume = 1)
+    {
+        instance.audioSource.PlayOneShot(instance.soundList[(int)sound], volume);
+    }
     void Update()
     {
         
